18975bca1455ad5ea6aaa962a7845d8f30f48774
Michael Smith <Grey-Pretzel@users.noreply.github.com>
2016-05-12 11:03:49 -0400

    Create start.bat

---
+++ start.bat
@@ -0,0 +1,8 @@
+@echo off
+@color AC
+:Main
+start "start.bat"
+goto Spam
+:Spam
+echo You've been trolled! You've been trolled! Yes you've probably been told. Trololololololololololololololol
+goto Spam
